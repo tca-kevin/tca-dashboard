b36d22c3d7a72d3c576ed90b00729260210b4322
Kevin Lim <zzkevinlim@gmail.com>
2024-11-20 15:24:05 +1300

    202411201524

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-main-layout>
+	<x-slot:description>
+	</x-slot:description>
+
+	<x-slot:styles>
+		<style>
+		</style>
+	</x-slot:styles>
+
+	<div>
+	</div>
+
+	<x-slot:scripts>
+		<script>
+		</script>
+	</x-slot:scripts>
+</x-main-layout>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,69 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import fs from "fs";
+import dotenv from "dotenv";
 
-export default defineConfig({
+const env = dotenv.config().parsed;
+
+const commonConfig = {
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                "resources/sass/app.scss",
+                "resources/css/app.css",
+                "resources/js/app.js",
+            ],
+            refresh: [...refreshPaths],
         }),
     ],
-});
+};
+
+let config = {};
+
+config = {
+    ...commonConfig,
+};
+
+if (env?.DEV_ENV === "docker") {
+    config = {
+        server: {
+            host: "0.0.0.0",
+            port: 5173,
+            hmr: {
+                host: env?.APP_HOST,
+            },
+            https: {
+                key: fs.readFileSync(`${env.APP_KEY_DIR}`),
+                cert: fs.readFileSync(`${env.APP_CERT_DIR}`),
+            },
+            watch: {
+                usePolling: true,
+            },
+        },
+        ...commonConfig,
+    };
+} else if (env?.DEV_ENV === "valet") {
+    config = {
+        server: {
+            host: "127.0.0.1",
+            port: 5173,
+            hmr: {
+                host: env?.APP_HOST,
+            },
+            https: {
+                key: fs.readFileSync(`${env.APP_KEY_DIR}`),
+                cert: fs.readFileSync(`${env.APP_CERT_DIR}`),
+            },
+            watch: {
+                usePolling: true,
+            },
+        },
+        ...commonConfig,
+    };
+} else {
+    config = {
+        ...commonConfig,
+    };
+}
+
+export default defineConfig(config);
